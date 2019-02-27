8f6412a7da2a6fd66df9a7374a5804800317fba9
Ech <everts.chavez@sincosoftware.com>
2019-02-26 22:57:30 -0600

    Initial Commit

---
+++ ecwizardlibrary/src/main/java/com/evertschavez/ecwizardlibrary/model/SimpleLocationListener.java
@@ -0,0 +1,7 @@
+package com.evertschavez.ecwizardlibrary.model;
+
+import android.location.Location;
+
+public interface SimpleLocationListener {
+	public void onLocationChanged(Location location);
+}
